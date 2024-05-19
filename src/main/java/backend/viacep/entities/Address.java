2096c66bf4517ca6d6b3a0e6fcb77c5f256a51b6
Wesley <Wesleyscorrea@hotmail.com>
2024-05-19 16:13:53 -0300

    Add enti

---
+++ src/main/java/backend/viacep/entities/Address.java
@@ -0,0 +1,42 @@
+package backend.viacep.entities;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "address")
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "cep", unique = true)
+    private String cep;
+    @Column(name = "logradouro")
+    private String logradouro;
+    @Column(name = "complemento")
+    private String complemento;
+    @Column(name = "bairro")
+    private String bairro;
+    @Column(name = "localidade")
+    private String localidade;
+    @Column(name = "uf")
+    private String uf;
+    @Column(name = "ibge")
+    private String ibge;
+    @Column(name = "gia")
+    private String gia;
+    @Column(name = "ddd")
+    private String ddd;
+    @Column(name = "siafi")
+    private String siafi;
+}
---
+++ src/main/java/backend/viacep/entities/Users.java
@@ -0,0 +1,34 @@
+package backend.viacep.entities;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "user_id")
+    private List<Address> address;
+}
